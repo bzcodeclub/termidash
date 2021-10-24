4ead14b1112f3d97cc7524d8bea04db6d59173f9
Zekeriya AY <zekeriya@zekeriyaay.com>
2021-10-24 23:32:32 +0300

    todo Added

--- src/termidash.cpp
+++ src/termidash.cpp
@@ -1,3 +1,33 @@
-int main() {
+#include "termidash.h"
+
+using namespace std;
+
+int main()
+{
+    fstream todoFile;
+    todoFile.open("todo.td", ios::in);
+
+    string todoLine;
+    int line = 1;
+
+    initscr();
+
+    /* ToDo Box */
+    WINDOW *todoWin = newwin(15, 20, 1, 2);
+    box(todoWin, 0, 0);
+    mvwprintw(todoWin, 0, 2, " ToDo ");
+    refresh();
+
+    while (getline(todoFile, todoLine)) {
+        mvwprintw(todoWin, line, 1, todoLine.c_str());
+
+        line++;
+    }
+    wrefresh(todoWin);
+
+    todoFile.close();
+
+    getch();
+    endwin();
     return 1;
 }
