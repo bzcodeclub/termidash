67b3971b3f3ecfb4b48790aae311eefd3122821d
Berkay Çubuk <berkaycubuk@gmail.com>
2021-10-24 22:56:48 +0300

    Todos are coming from the string array

--- src/termidash.cpp
+++ src/termidash.cpp
@@ -1,5 +1,30 @@
+#include <iostream>
 #include <ncurses.h>
 
 int main() {
+    char *todos[] = {
+        "1 kilo domates",
+        "Buzdolabı poseti al",
+        "Algoritma analizi sunumunu hazırla"
+    };
+
+    // start curses mode
+    initscr();
+
+    printw("TermiDash v1.0.0");
+    printw("\n\n\nYapılacaklar:\n");
+
+    for (int i = 0; i < sizeof(todos) / sizeof(todos[0]); i++) {
+        printw("- %s\n", todos[i]);
+    }
+
+    refresh();
+
+    // keep it open until someone presses a key
+    getch();
+
+    // close the curses
+    endwin();
+
     return 1;
 }
